f5a68d76a86962e77bfc0261aef965bbc4f12919
danielhumgon <danielhumgon@gmail.com>
2019-08-07 12:37:50 -0400

    Refactored address-details example to use bchjs

--- examples/low-level/address-details/address-details.js
+++ examples/low-level/address-details/address-details.js
@@ -4,16 +4,18 @@
 */
 
 // Instantiate BITBOX.
-const bitboxLib = "../../../lib/BITBOX"
-const BITBOXSDK = require(bitboxLib)
-const BITBOX = new BITBOXSDK({ restURL: "https://trest.bitcoin.com/v2/" })
+//const bitboxLib = "../../../lib/BITBOX"
+//const BITBOXSDK = require(bitboxLib)
+//const BITBOX = new BITBOXSDK({ restURL: "https://trest.bitcoin.com/v2/" })
+const BCHJS = require("@chris.troutner/bch-js")
+const bchjs = new BCHJS({ restURL: `http://decatur.hopto.org:12400/v3/` })
 
-const ADDR = `bchtest:qr45kxqda7yw8atztvkc4ckqnrlhmp0kvsep4p345q`
-
+//const ADDR = `bchtest:qr45kxqda7yw8atztvkc4ckqnrlhmp0kvsep4p345q`
+const ADDR = "1BFHGm4HzqgXXyNX8n7DsQno5DAC4iLMRA"
 async function addressDetails() {
   try {
     // first get BCH balance
-    const balance = await BITBOX.Address.details(ADDR)
+    const balance = await bchjs.Insight.Address.details(ADDR)
 
     console.log(`BCH Balance information:`)
     console.log(balance)
